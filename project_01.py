dacab026a868f75453abbe5cc50f624bf53a53fe
Giles <giles.cooper@gmail.com>
2024-03-19 00:01:37 -0400

    begin week 1 project

---
+++ project_01.py
@@ -0,0 +1,21 @@
+"""
+The file UN.csv contains the names and some details of each country that 
+    belongs to the UN.  Each line of the input file contains the record
+    of one country -- each record contains 4 items separated by a comma 
+    as the delimiter. The items in each record are in this order:
+
+        country
+        continent
+        population in millions
+        area in square miles
+
+The goal of your program is to use this input file to create an output file
+    containing only the country and the population.  The output file should 
+    be ordered by total population (descending order).
+
+Be sure that your program file contains a comment at the top with your name, 
+    contains well-named variables and is commented to indicate the tasks that
+    are being completed.
+
+Output is also to be clearly formatted  (see sample output file provided)
+"""
