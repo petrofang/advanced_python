94cef955d4f9ee5626a0b1bcce1a8476bd937c47
Giles <giles.cooper@gmail.com>
2024-03-18 23:46:14 -0400

    complete exercise 2

--- week01-ex2.py
+++ week01-ex2.py
@@ -1,6 +1,5 @@
 # Week 1 Ex 2
 # Giles Cooper
-
 """
 The purpose of this exercise is to give you some practice using the csv module
     of Python.  This module provides a convenient way to work with csv file, 
@@ -33,3 +32,26 @@ Be sure that your program file contains a comment at the top with your name,
 """
 
 import csv
+short_term_dems=[]
+short_term_reps=[]
+
+# find short-term presidents and sort by party.
+with open('presidential.csv', 'r') as presidents:
+    president_list = csv.DictReader(presidents)
+    for president in president_list: 
+        short_term=False       
+        if not president["start"].startswith("1/20"): short_term=True
+        if not president["end"].startswith("1/20"): short_term=True
+        if short_term: 
+            if president["party"] == "Democratic": 
+                short_term_dems.append(president["name"])
+            elif president["party"] == "Republican": 
+                short_term_reps.append(president["name"])
+
+with open('partial_term_presidents.txt', 'w') as part:
+    part.write("Democratic Presidents:\n")
+    for president in short_term_dems:
+            part.write(f'{president} served an incomplete term.\n')  
+    part.write("\nRepublican Presidents:\n")
+    for president in short_term_reps:
+            part.write(f'{president} served an incomplete term.\n')
