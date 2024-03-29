472d23c3e5b315a4ed71bf2896b1db95583e28cc
Giles <giles.cooper@gmail.com>
2024-03-29 18:29:42 -0400

    complete project 2.

--- week02/week02_project.py
+++ week02/week02_project.py
@@ -3,7 +3,12 @@ Store Employee Database and Payroll Program
 (C) Giles Cooper 2024
 MIT License
 """
+import pickle
+
 import payroll
+
+DEFAULT_DB_NAME = 'payroll.db'
+
 menu = {
     "M":"Add New Manager",
     "A":"Add New Associate",
@@ -15,6 +20,7 @@ menu = {
     "I":"Print info on a worker",
     "S":"Print a manager's Salary",
     "P":"Print an associate's Pay",
+    "Q":"Save and Quit"
 }
 
 #dictionary to ease access to StoreWorker employee_number
@@ -22,6 +28,8 @@ menu = {
 managers={}
 associates={}
 
+# we end up listing workers a lot, the following functions help with that.
+# sometimes we want just the managers, sometimes just associates
 def print_managers():
     for id, worker in managers.items(): print(f"{id} : {worker.name}")
 
@@ -29,8 +37,11 @@ def print_associates():
     for id, worker in associates.items(): print(f"{id} : {worker.name}")
 
 def print_workers():
-    workers = {**associates, **managers}
-    for id, worker in workers.items(): print(f"{id} : {worker.name}")
+    # sometimes both. Smooth them together and re-sort them.
+    workers = dict(sorted({**associates, **managers}.items()))
+    if not workers: print("No workers have been entered.")
+    else:
+        for id, worker in workers.items(): print(f"{id} : {worker.name}")
 
 # create a new worker of type "A" or "M"
 def create_new_worker(worker_type):
@@ -62,8 +73,7 @@ def give_raise(worker_type):
         print_managers()
     elif worker_type == 'r':
         period = "hour"
-        for id, worker in associates.items(): 
-            print(f"{id} : {worker.name}")
+        print_associates()
     worker=input("  >>  ")
 
     # validate worker number and worker type
@@ -95,6 +105,11 @@ def give_raise(worker_type):
             workers[worker].give_raise(raise_)
             print(f"Done. {workers[worker]}")
 
+# increment years, based on worker type.
+# since both manager and associate have the same method to
+# increase their tenure, this distinction is unnecessary,
+# but it was written as two seperate bullet points in 
+# Part 2 of the week 2 project details
 def increment_years(worker_type):
     if worker_type == "Y":
         print_managers()
@@ -113,16 +128,85 @@ def increment_years(worker_type):
         workers = {**associates, **managers}
         workers[worker].increase_years()
 
+#print worker info
+def print_info():
+    print_workers()
+    print("Which worker would you like information on? (by number)")
+    try:
+        worker_number = int(input())
+        workers = {**associates, **managers}
+        if worker_number not in workers:
+            raise IndexError
+    except:
+        print("not a valid worker number")
+    else:
+        print(workers[worker_number])
+    
+# each function I am more and more annoyed at my own coding pattern
+# but at this point I'm invested so might as well just finish it
+def print_pay(worker_type):
+    if worker_type == "S":
+        period="week"
+        print_managers()
+    elif worker_type == "P":
+        period="hour"
+        print_associates()
+    print("Which worker would you like to see payroll info? (by number)")
+    worker = input()
+    try: 
+        worker=int(worker)
+        if worker_type == "S" and worker not in managers.keys(): 
+            raise KeyError
+        if worker_type == "P" and worker not in associates.keys(): 
+            raise KeyError
+    except: print("Invalid worker.")
+    else: 
+        workers = {**associates, **managers}
+        if worker_type == "S":
+            pay = workers[worker].get_salary()
+        elif worker_type == "P":
+            pay = workers[worker].get_hourly_rate()
+        # finally a decent line of code:
+        print(f"{workers[worker].name}: ${pay}/{period}")
+            
+# save payroll database to file
+def save_data():
+    workers = {**associates, **managers}
+    try:
+        with open('payroll.db', 'wb') as payroll_db:
+            pickle.dump(workers, payroll_db)
+    except:
+        print("database could not be saved for whatever reason.")
+    
+# load payroll database from file:
+def load_data():
+    print("Please enter database file name to load...")
+    if input() != DEFAULT_DB_NAME: print("incorrect.")
+    try:
+        with open(DEFAULT_DB_NAME, 'rb') as payroll_db:
+            db = pickle.load(payroll_db)
+            for id in db:
+                if isinstance(db[id], payroll.Associate):
+                    associates[id]=db[id]
+                elif isinstance(db[id], payroll.Manager):
+                    managers[id]=db[id]
+        print("Previous payroll data loaded...")
+    except:
+        pass # say nothing if no data found
+
 def main():
+    load_data()
     while True:
-        for key,val in menu.items():
-            print(f"{key} ). {val} ")
+        # display options menu:
+        for key,val in menu.items(): print(f"{key} ). {val} ")
         option = input("Select from menu:  >>  ")
+
         if option not in menu.keys():
             print("command not recognized...")
             if option.lower() in menu.keys() or option.upper() in menu.keys():
                 print("input is case-sensitive!")
             continue
+
         elif option == "M" or option == "A":
             create_new_worker(option)
         elif option == "R" or option == "r":
@@ -131,11 +215,13 @@ def main():
             increment_years(option)
         elif option == "L":
             print_workers()
+            input("(press Enter to continue)")
         elif option == "I":
-            pass # TODO: Print info on a worker
-        elif option == "S":
-            pass # TODO: Print a manager's Salary
-        elif option == "P":
-            pass # TODO: Print an associate's Pay
-
+            print_info()
+        elif option == "S" or option == "P":
+            print_pay(option)
+        elif option == "Q":
+            print("Okay bye")
+            save_data()
+            break
 if __name__=="__main__": main()
