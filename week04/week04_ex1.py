87b9873ac657c6abf11a6ddbca05ea08a6033133
Giles <giles.cooper@gmail.com>
2024-04-09 17:19:29 -0400

    begin week 4 exercise 1

---
+++ week04/week04_ex1.py
@@ -0,0 +1,31 @@
+# Giles Cooper 2024
+"""The file USPopulation.txt contains the midyear population of the 
+    United States, in thousands, during the years 1950 through 1990.  
+    The first line of the file contains the population for 1950, 
+    the second for 1951 and so forth. 
+
+Write a program which reads the populations from the file and calculates
+    the following:
+
+The program should output:
+
+The average annual change in population during that time period
+The year with the greatest increase in population during that time period
+The year with the smallest increase in population during that time period
+The program should provide a line plot of annual changes in populations
+    from 1950 - 1990. Be sure that your plot contains a title, x and y labels,
+    some color and an attractive marker.
+Your program should use only ndarrays for storage of populations, years
+    and population changes.
+Be sure your code well commented.
+
+Hint:  You may want to develop the program incrementally, verifying array 
+    contents and calculations with output (or the variable window) after each 
+    task is completed.
+
+Output:
+    Average change is 2443.88
+    Largest increase in population 3185 in year 1955
+    Smallest increase in population 1881 in year 1967
+"""
+
