f9136fab724778e1ba43379796a2543b8aa7d472
Giles <giles.cooper@gmail.com>
2024-04-17 15:33:16 -0400

    begin week 05: pandas

---
+++ week05/week05_ex1b.py
@@ -0,0 +1,38 @@
+# Giles Cooper
+"""8.) Create a Data Frame from the following dictionary:
+
+data = { 'Course': [ 'Financial Accounting', 'Microeconomics', 
+    'Programming Fundamentals',  'Statistics', 'Data Science'],  
+    'No of Students': [150, 75, 200, 175, 125], 
+    'Department': ['Accountancy', 'Economics', 'CIS', 'Math', 'Math' ]  }
+
+9.) Display the DataFrame
+
+10.) Display only the Course and Department columns
+
+11.) Output the rows 0,3,4 from the Course and Department columns.
+
+12.) Add code to count the number of rows and columns.
+
+13.) Add a column labeled Professor, containing professor
+    names 'Brown', 'Griffin', 'Sanchez', 'Chen', 'Pavone' 
+    and display the updated DataFrame.
+
+14.) Add a row containing the course 'Discrete Math', with 65 students,
+    Math department and professor 'Hasson' and display the updated DataFrame.
+
+15.) Add a column labeled Capacity, containing all 200's  
+    and display the updated DataFrame.
+
+16.) Change the column label “No of Students” to simply “Students”.
+
+17.) Add a column labeled Avail, containing the space in all classrooms 
+    and display the updated DataFrame.
+
+18.) Using the DataFrame, output the average class size.
+
+19.) Using the DataFrame, output the name of the class 
+    with the largest available seats.
+
+20.)  Delete all data for the course “Financial Accounting” 
+    from the DataFrame. Display the updated DataFrame."""
