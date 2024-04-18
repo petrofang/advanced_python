b106c4632588d9c5c944726655650c94bbb062b2
Giles <giles.cooper@gmail.com>
2024-04-18 16:21:52 -0400

    exercise 2 #1

--- week05/week02-ex2.py
+++ week05/week02-ex2.py
@@ -1,1 +1,8 @@
 # Giles Cooper
+import pandas as pd
+
+#1
+df = pd.DataFrame({'Apples': [150, 175, 200, 125],
+                   'Pears': [250, 135, 325, 645],
+                   'Bananas': [250, 135, 325, 645]})
+
